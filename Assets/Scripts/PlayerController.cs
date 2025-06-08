e776b85efde11977b75a3f2989bfe54be4f302a8
Artyom19 <masterArtem5000@gmail.com>
2025-06-08 20:39:03 +0500

    Add simple shooting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,12 @@ public class PlayerController: MonoBehaviour
     private Rigidbody2D rb;
     public float speed = 5f;
     public int health = 10;
+    
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public float bulletForce = 20f;
+    public float fireRate = 0.5f;
+    private float nextFireTime = 0f;
 
     private void Start()
     {
@@ -13,6 +19,16 @@ public class PlayerController: MonoBehaviour
     }
 
     private void Update()
+    {
+        Move();
+        if (Input.GetKeyDown(KeyCode.Mouse0) && Time.time >= nextFireTime)
+        {
+            nextFireTime = Time.time + fireRate;
+            Shooting();
+        }
+    }
+
+    private void Move()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
@@ -26,5 +42,17 @@ public class PlayerController: MonoBehaviour
         {
             rb.linearVelocity = Vector2.zero;
         }
+    }
+
+    private void Shooting()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = (mousePos - (Vector2)firePoint.position).normalized;
+        
+        rb.AddForce(direction * bulletForce, ForceMode2D.Impulse);
     }
 }
