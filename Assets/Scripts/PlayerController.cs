a804f36098f1e8573bf14f3e43a9a73ba6f69bce
Artyom19 <masterArtem5000@gmail.com>
2025-06-08 20:04:52 +0500

    Add player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,30 @@
+using System;
 using UnityEngine;
 
 public class PlayerController: MonoBehaviour
 {
-    
+    private Rigidbody2D rb;
+    public float speed = 5f;
+    public int health = 10;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        
+        Vector2 movement = new Vector2(horizontal, vertical).normalized * speed;
+        if (movement != Vector2.zero)
+        {
+            rb.linearVelocity = movement;
+        }
+        else
+        {
+            rb.linearVelocity = Vector2.zero;
+        }
+    }
 }
